119df4748524e8320a56607f6c52bc4af6038b0c
Peter Czibik <p.czibik@gmail.com>
2015-11-24 14:15:27 +0100

    move config file in product api

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mainApiUrl: process.env.MAINAPIURL || 'http://localhost:3200',
+  mainApiUrl: process.env.MAIN_API_URL || 'http://localhost:3200',
   port: process.env.PORT || 31337,
   jwtSecret: process.env.JWT_SECRET || 'jwtSecret'
 }
