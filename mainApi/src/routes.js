42b7665e4560a356b732c0d894d261d06e549e3c
Peter Czibik <p.czibik@gmail.com>
2015-11-25 07:23:49 +0100

    removed training material

--- mainApi/src/routes.js
+++ mainApi/src/routes.js
@@ -1,24 +1,17 @@
 'use strict'
 
 const Router = require('koa-router')
-const jwt = require('koa-jwt')
-const config = require('../config')
-
-const jwtMiddleware = jwt({
-  secret: config.jwtSecret,
-  key: 'admin'
-})
 
 const router = new Router()
 
 /* secured */
-router.get('/users', jwtMiddleware, function * () {
+router.get('/users', function * () {
 })
-router.get('/users/:userId', jwtMiddleware, function * () {
+router.get('/users/:userId', function * () {
 })
-router.get('/users/:userId/products', jwtMiddleware, function * () {
+router.get('/users/:userId/products', function * () {
 })
-router.post('/users/:userId/products', jwtMiddleware, function * () {
+router.post('/users/:userId/products', function * () {
 })
 
 module.exports = router
