7b2873641f4608d908f5d166a31f311262c9c954
Peter Czibik <p.czibik@gmail.com>
2016-01-19 16:09:37 +0100

    Freeze packages and add cors

--- mainApi/src/index.js
+++ mainApi/src/index.js
@@ -2,8 +2,11 @@ const koa = require('koa')
 const router = require('./routes')
 const logger = require('winston')
 const config = require('../config')
+const cors = require('koa-cors')
+
 const app = koa()
 
+app.use(cors())
 app.use(router.routes())
 app.listen(config.port, () => {
   logger.info('app is listening on port:', config.port)
--- usersApi/src/index.js
+++ usersApi/src/index.js
@@ -1,18 +1,15 @@
 'use strict'
 
 const Koa = require('koa')
-const request = require('superagent')
 const bodyParser = require('koa-bodyparser')
 const logger = require('winston')
-const mongoose = require('mongoose')
-const cors = require('koa-cors')
 
 const config = require('./config')
 const router = require('./routes')
 
 const app = new Koa()
+
 app.use(bodyParser())
-app.use(cors())
 
 app.use(router.middleware())
 app.listen(config.port, function () {
